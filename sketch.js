b00c20b98b6175e699ea02386e405f738832b8d8
Sausageexpert <68068569+Sausageexpert@users.noreply.github.com>
2020-08-22 09:38:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -29,17 +29,17 @@ function draw() {
    var deformation=0.5 * weight * speed* speed/22500;
    car.velocityX = 0;
 
-	if(deformation > 180)
+	if(deformation < 100)
 	{
-    car.shapeColor=color(0,0,255);
+    car.shapeColor=color(0,255,0);
   }
   
- if (deformation < 180){
-   car.shapeColor = color(0,255,0);
+ if (deformation > 100 && deformation < 180){
+   car.shapeColor = color(230, 230 ,0);
  }
 
  if (deformation === 180){
-   car.shapeColor = color(255,255,0);
+   car.shapeColor = color(255,0,0);
  }
 }
 
